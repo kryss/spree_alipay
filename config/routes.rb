b8db8efb7f71c8f62e32b9980e9678e5fd39326e
yakjuly <yakjuly@gmail.com>
2013-11-25 21:41:06 +0800

    change alipay

--- config/routes.rb
+++ config/routes.rb
@@ -12,7 +12,7 @@ Spree::Core::Engine.routes.draw do
 
   # Add your extension routes here
   get '/alipay_checkout/done/' => 'checkout#alipay_done', :as => :alipay_done
-  get '/alipay_checkout/notify/' => 'checkout#alipay_notify', :as => :alipay_notify
+  post '/alipay_checkout/notify/' => 'checkout#alipay_notify', :as => :alipay_notify
   # get '/alipay_checkout/done/' => 'checkout#alipay_done', :as => :alipay_done
   #  post '/alipay_checkout/done/' => 'checkout#alipay_done', :as => :alipay_done
 end
